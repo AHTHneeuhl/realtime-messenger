c35053b23289f9cb5403e4867442f2a2bd7d85b4
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-28 17:25:35 +0530

    chore: Refactor authConroller functions

--- packages/server/src/controllers/validateForm.ts
+++ packages/server/src/controllers/validateForm.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { formSchema } from "@messenger/common";
 
-export const validateForm = (req: Request, res: Response) => {
+export const validateForm = (req: Request, res: Response, next: Function) => {
   const formData = req.body;
   formSchema
     .validate(formData)
@@ -9,6 +9,9 @@ export const validateForm = (req: Request, res: Response) => {
       if (valid) {
         console.log("Valid");
         res.status(200).send();
+        next();
+      } else {
+        res.status(422).send();
       }
     })
     .catch((err) => {
--- packages/server/src/routes/authRouter.ts
+++ packages/server/src/routes/authRouter.ts
@@ -1,63 +1,17 @@
 import { Router } from "express";
 import { validateForm } from "../controllers/validateForm";
-import pool from "../db";
-import bcrypt from "bcrypt";
+import {
+  getSession,
+  handleSignIn,
+  handleSignUp,
+} from "../controllers/authControllers";
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {
-  validateForm(req, res);
+router.post("/signup", validateForm, handleSignUp);
 
-  const { username, password } = req.body;
+router.post("/signin", validateForm, handleSignIn);
 
-  const existingUser = await pool.query(
-    "SElECT username FROM users WHERE email = $1",
-    [req.body.username]
-  );
-
-  if (existingUser.rowCount === 0) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await pool.query(
-      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING username",
-      [username, hashedPassword]
-    );
-    req.session.user = {
-      username,
-      id: newUser.rows[0].id,
-    };
-    res.json({ loggedIn: true, username });
-  } else {
-    res.json({ loggedIn: false, status: "Username already exists" });
-  }
-});
-
-router.post("/signin", async (req, res) => {
-  validateForm(req, res);
-
-  const { username, password } = req.body;
-
-  const potentialLogin = await pool.query(
-    "SELECT id, username, password FROM users WHERE username = $1",
-    [username]
-  );
-
-  if (potentialLogin.rowCount > 0) {
-    const verifiedPassword = await bcrypt.compare(
-      password,
-      potentialLogin.rows[0].password
-    );
-    if (verifiedPassword) {
-      req.session.user = {
-        username,
-        id: potentialLogin.rows[0].id,
-      };
-      res.json({ loggedIn: true, username });
-    } else {
-      res.json({ loggedIn: false, status: "Username does not exist" });
-    }
-  } else {
-    res.json({ loggedIn: false, status: "Username does not exist" });
-  }
-});
+router.get("/signin", getSession);
 
 export default router;
